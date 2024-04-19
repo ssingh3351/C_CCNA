f8e042ea8a68acce67e529fe255334b61b36d725
Shailendra Singh <Ss5661@outlook.com>
2024-04-19 14:25:59 +0530

    infix to postfix A & L

---
+++ VisualStudio/Infix_to_postfix_with_A_and_P/Infix_to_postfix_with_A_and_P/Infix_to_postfix_with_A_and_P.cpp
@@ -0,0 +1,12 @@
+// Infix_to_postfix_with_A_and_P.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+
+
+
+int main()
+{
+    cout << "Hello World!\n";
+}
